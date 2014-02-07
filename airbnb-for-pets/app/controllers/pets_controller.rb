cb6f250d346bff26af4bb9aa19b6b1a28fe81b52
Shawna Mattison <shawnamatt@gmail.com>
2014-02-06 22:23:52 -0800

    Add bootstrap and some custom styling to pets index page.

--- airbnb-for-pets/app/controllers/pets_controller.rb
+++ airbnb-for-pets/app/controllers/pets_controller.rb
@@ -7,7 +7,25 @@ class PetsController < ApplicationController
 		@pet = Pet.new
 	end
 
+	def show
+		@pet = Pet.find(params[:id])
+	end
+
 	def create
 		@pet = Pet.new(pet_params)
+
+		respond_to do |format|
+			if @pet.save
+				format.html { redirect_to @pet, notice: 'Pet was successfully created.' }
+				format.json { render action: 'show', status: :created, location: @pet }
+			else
+				format.html { render action: 'new' }
+				format.json { render json: @pet.errors, status: :unprocessable_entity }
+			end
+		end
+	end
+
+	def pet_params
+		params.require(:pet).permit(:name, :description)
 	end
 end
--- airbnb-for-pets/app/models/pet.rb
+++ airbnb-for-pets/app/models/pet.rb
@@ -1,2 +1,4 @@
 class Pet < ActiveRecord::Base
+
+	
 end
