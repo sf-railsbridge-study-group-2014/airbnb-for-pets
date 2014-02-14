e4ad8769a9c76eb730f067e6040c8f316436de54
Zee Spencer <zee@zeespencer.com>
2014-02-14 15:31:48 -0800

    Merge Shawna's branch of Release 1
    Squashed commit of the following:
    
    commit c47070b63fd3d3795775e8b4c1831f89e2b57b75
    Author: Zee Spencer <zee@zeespencer.com>
    Date:   Fri Feb 14 15:28:16 2014 -0800
    
        Fix link to add a pet
    
        * Buttons don't appear to be able to hold links.
        * Indentation should use spaces, not tabs
    
    commit 95d3a5c65b4438487024292b9cdf93e1a3627cde
    Author: Shawna Mattison <shawnamatt@gmail.com>
    Date:   Wed Feb 12 20:48:46 2014 -0800
    
        Extra validation on new pet page to prevent guest user from creating pets
    
    commit c1691d4639ae8ba647aa1f416dcd0b77615dab09
    Author: Shawna Mattison <shawnamatt@gmail.com>
    Date:   Wed Feb 12 20:33:34 2014 -0800
    
        Only signed in users can add a pet
    
    commit 689b7740a5f546728a6b1f45ac5d43738ea1337f
    Author: Shawna Mattison <shawnamatt@gmail.com>
    Date:   Wed Feb 12 20:31:51 2014 -0800
    
        Add sign in, sign up, log out and edit profile links in navbar
    
    commit b00f99abab6ce7462bcac7505978753c9d11ea04
    Author: Shawna Mattison <shawnamatt@gmail.com>
    Date:   Wed Feb 12 20:04:44 2014 -0800
    
        Generate devise user model
    
    commit cdf26de5285f3e2869c3a3961a894dfa90b37b6c
    Author: Shawna Mattison <shawnamatt@gmail.com>
    Date:   Wed Feb 12 20:02:42 2014 -0800
    
        Install devise and do basic configuration. Generate devise views
    
    commit 6bc7cbd01abf00f93e59e8a953b81e5fd660d5e4
    Author: Shawna Mattison <shawnamatt@gmail.com>
    Date:   Mon Feb 10 20:50:42 2014 -0800
    
        More styling
    
    commit cb6f250d346bff26af4bb9aa19b6b1a28fe81b52
    Author: Shawna Mattison <shawnamatt@gmail.com>
    Date:   Thu Feb 6 22:23:52 2014 -0800
    
        Add bootstrap and some custom styling to pets index page.
    
    commit 65d226dc7b662019901a144154205b9cba5792b2
    Author: Shawna Mattison <shawnamatt@gmail.com>
    Date:   Thu Feb 6 22:00:47 2014 -0800
    
        Finish ability to create pets by adding create pet action and show pet page.
    
    commit 852d855087fdae95a2b3d627afd9c79df1d65275
    Author: Shawna Mattison <shawnamatt@gmail.com>
    Date:   Thu Feb 6 21:39:01 2014 -0800
    
        Add new pet action, view and pets form. Make 'All Pets' page the homepage.
    
    commit 4fa17e649aa671e693007a2450878b828b13fb6d
    Author: Shawna Mattison <shawnamatt@gmail.com>
    Date:   Thu Feb 6 21:12:32 2014 -0800
    
        Create index action for pets and the index view.
    
    commit 8b7643545bcf1f8f16f701a3f140ccd72a772409
    Author: Shawna Mattison <shawnamatt@gmail.com>
    Date:   Thu Feb 6 21:06:31 2014 -0800
    
        Generate pets model and controller. Create routes for pets resource.

---
+++ airbnb-for-pets/app/controllers/pets_controller.rb
@@ -0,0 +1,35 @@
+class PetsController < ApplicationController
+	def index
+		@pets = Pet.all
+	end
+
+	def new
+		@pet = Pet.new
+	end
+
+	def show
+		@pet = Pet.find(params[:id])
+	end
+
+	def create
+		if !user_signed_in?
+			redirect_to new_user_session_path
+		else
+			@pet = Pet.new(pet_params)
+
+			respond_to do |format|
+				if @pet.save
+					format.html { redirect_to @pet, notice: 'Pet was successfully created.' }
+					format.json { render action: 'show', status: :created, location: @pet }
+				else
+					format.html { render action: 'new' }
+					format.json { render json: @pet.errors, status: :unprocessable_entity }
+				end
+			end
+		end
+	end
+
+	def pet_params
+		params.require(:pet).permit(:name, :description)
+	end
+end
